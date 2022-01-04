2f0ba5ed0e15beccd9e76d4d136d9767a2fef1a4
Gavdzinskyi <veacheslavgavdzynsky@gmail.com>
2022-01-04 19:39:12 +0200

    Added all files at once, training site

---
+++ itproger/news/forms.py
@@ -0,0 +1,37 @@
+from .models import Articles
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
+
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['title', 'anons', 'full_text', 'date', 'time_create']
+
+        widgets = {
+
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название статьи'
+            }),
+
+            "anons": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс статьи'
+            }),
+
+            "date": DateTimeInput(attrs={
+                'type': 'date',
+                'class': 'form-control',
+            }),
+
+            "time_create": DateTimeInput(attrs={
+                'type': 'time',
+                'class': 'form-control',
+            }),
+
+            "full_text": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Текст статьи'
+            })
+
+        }
